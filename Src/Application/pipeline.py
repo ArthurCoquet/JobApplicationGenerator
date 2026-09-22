ab112df9df9810cab569bd9b3fbed1c257850428
Arthur Coquet <arthur.coquet@hotmail.fr>
2026-09-23 00:16:05 +0200

    feat: simplifying pipeline.py

--- Src/Application/pipeline.py
+++ Src/Application/pipeline.py
@@ -6,45 +6,43 @@ async def pipeline(
         job_offer: JobOffer
     ):    
 
+    def checkpoint() -> None:
+        application.offer_repo.save(
+            path=offer_path,
+            data=job_offer.model_dump(),
+        )
+
     offer_path = rf"Offers\{job_offer.title}.json"
 
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     application.analyzer.analyze(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     await application.retriever.retrieve_experience(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     application.reranker.rerank_experiences(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     should_write = application.writing_decider.do_write(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     if not should_write:
         return
 
     application.offer_enricher.add_exp_and_facts(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     application.resume_writer.write_resume(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     #render resume
 
     application.letter_writer.write_cover_letter_plan(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
     application.letter_writer.write_cover_letter(job_offer=job_offer)
-
-    application.offer_repo.save(path=offer_path, data=job_offer.model_dump())
+    checkpoint()
 
 
